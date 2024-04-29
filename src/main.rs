3eeb082177921bf6caa6f871f3a3eef500fade64
John Krueger <johnathankrueger@me.com>
2024-04-28 21:22:45 -0400

    add test file and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path: std::path::PathBuf = std::env::args().nth(1).expect("no input provided").into();
-    let content = std::fs::read_to_string(&path).expect("could not read file");
-    for line in content.lines() {
-        println!("{}", line);
-    }
+    let content = std::fs::read_to_string(&path)?;
+    println!("{}", content);
+    Ok(())
 }
