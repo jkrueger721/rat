65077e5f22bf823955a8271c8df75e36f5d07e61
John Krueger <johnathankrueger@me.com>
2024-04-28 21:31:22 -0400

    refactor for better error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
     let path: std::path::PathBuf = std::env::args().nth(1).expect("no input provided").into();
-    let content = std::fs::read_to_string(&path)?;
+    let content = std::fs::read_to_string(&path)
+        .with_context(|| format!("could not read file `{}`", path.display()))?;
     println!("{}", content);
     Ok(())
 }
